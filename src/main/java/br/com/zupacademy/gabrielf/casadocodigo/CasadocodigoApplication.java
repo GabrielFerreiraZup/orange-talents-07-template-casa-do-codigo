0b406019de09f05894060cf8a07b405c7b7c0f67
GabrielFerreiraZup <gabriel.mendes@zup.com.br>
2021-07-27 18:30:11 -0300

    commit de setup

---
+++ src/main/java/br/com/zupacademy/gabrielf/casadocodigo/CasadocodigoApplication.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.gabrielf.casadocodigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasadocodigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasadocodigoApplication.class, args);
+	}
+
+}
