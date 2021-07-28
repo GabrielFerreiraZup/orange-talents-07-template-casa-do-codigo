4e4a24e19970ea45aec0632995ffa967b42803f9
GabrielFerreiraZup <gabriel.mendes@zup.com.br>
2021-07-28 10:30:24 -0300

    cadastro autor ok

--- src/main/java/br/com/zupacademy/gabrielf/casadocodigo/repository/AutorRepository.java
+++ src/main/java/br/com/zupacademy/gabrielf/casadocodigo/repository/AutorRepository.java
@@ -2,7 +2,9 @@ package br.com.zupacademy.gabrielf.casadocodigo.repository;
 
 import br.com.zupacademy.gabrielf.casadocodigo.modelo.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AutorRepository extends JpaRepository<Autor,Long> {
 
 }
